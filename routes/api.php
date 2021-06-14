081bf8074bfa798b6fc1f19ea1d11bd179576390
Ahmad Jarir A <ajax.soft.en@gmail.com>
2021-06-14 21:57:02 +0700

    update: add api to get geojson mapping

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +19,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/world-heritage-list', function () {
+//    $response = Http::get('https://examples.opendatasoft.com/explore/dataset/world-heritage-unesco-list/download?format=geojson');
+//
+//    if ($response->successful()) {
+//        return response()->json($response->json());
+//    }
+//
+//    return response()->json(['message' => 'Failed'], 404);
+
+    $file = File::get(public_path('assets/geojson/world-heritage-unesco-list.geojson'));
+
+    return response()->json(json_decode($file, true, 512, JSON_THROW_ON_ERROR));
+})->name('api.world-heritage-list');
